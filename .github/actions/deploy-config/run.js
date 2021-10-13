9ba597cd5b17786bb297f8a8ffdec8b830d5c5bc
Hidetake Iwata <int128@gmail.com>
2021-10-14 07:34:28 +0900

    refactor: rewrite matrix jobs with flat job (#18)

--- .github/actions/deploy-config/run.js
+++ .github/actions/deploy-config/run.js
@@ -1,8 +1,10 @@
 module.exports = ({ github, context, core }) => {
   core.info(`Received event ${context.eventName} at ref ${context.ref}`)
   const outputs = determineConfig(context)
-  core.info(`Set outputs to ${JSON.stringify(outputs, undefined, 2)}`)
-  core.setOutput('environments', JSON.stringify(outputs.environments))
+  for (const [k, v] of Object.entries(outputs)) {
+    core.info(`Set output ${k} = ${v}`)
+    core.setOutput(k, v)
+  }
 }
 
 const determineConfig = (context) => {
@@ -10,28 +12,15 @@ const determineConfig = (context) => {
 
   if (eventName === 'pull_request') {
     return {
-      environments: [
-        {
-          overlay: `pr`,
-          namespace: `pr-${context.issue.number}`,
-        },
-      ],
+      overlay: `pr`,
+      namespace: `pr-${context.issue.number}`,
     }
   }
 
   if (eventName === 'push' && ref === 'refs/heads/main') {
     return {
-      environments: [
-        {
-          overlay: `development`,
-          namespace: `development`,
-        },
-        {
-          overlay: `pr`,
-          namespace: '${NAMESPACE}', // will be replaced on pull-request--deploy workflow
-          prebuilt: true,
-        },
-      ],
+      overlay: `development`,
+      namespace: `development`,
     }
   }
 
